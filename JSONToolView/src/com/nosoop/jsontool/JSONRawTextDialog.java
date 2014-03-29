6a53d2e23bdf8b7b802ee82ad6fb019a34bb6209
nosoop <nosoop@users.noreply.github.com>
2014-03-28 22:34:45 -0700

    Enabled closing by pressing the dialog button.

--- JSONToolView/src/com/nosoop/jsontool/JSONRawTextDialog.java
+++ JSONToolView/src/com/nosoop/jsontool/JSONRawTextDialog.java
@@ -59,6 +59,11 @@ public class JSONRawTextDialog extends javax.swing.JDialog {
         jsonTextArea.setText(stringData);
 
         jButton1.setText("Close");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -88,6 +93,10 @@ public class JSONRawTextDialog extends javax.swing.JDialog {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
